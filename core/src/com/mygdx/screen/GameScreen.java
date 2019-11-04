8e1c06123e0d1b447b6c69f995298167b74a02d3
David Fletcher <dfletch1@binghamton.edu>
2019-11-04 09:59:18 -0500

    first commit, some work done

---
+++ core/src/com/mygdx/screen/GameScreen.java
@@ -0,0 +1,59 @@
+package com.mygdx.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.galaga.GalagaClass;
+
+public class GameScreen implements Screen {
+    public static final float SPEED = 30;
+    Texture img;
+    float x;
+    float y;
+
+    GalagaClass game;
+
+    public GameScreen(GalagaClass game){
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
